cb2ae951980e03a37561996f5b88407ab8ef9693
lantianx <128837704+lantianx233@users.noreply.github.com>
2025-07-18 20:28:26 +0800

    fix(build): lock nixpkgs version(25.05)

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   description = "nixos flake config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nur = {
--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -13,8 +13,10 @@
   };
 
   # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  # services.displayManager.gdm.enable = true;
+  # services.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
