e7c6cff764036acfe92bee33ae7c11668c1842fb
lantianx <128837704+lantianx233@users.noreply.github.com>
2025-07-21 07:30:18 +0800

    chore: switch to unstable channel

--- flake.nix
+++ flake.nix
@@ -2,10 +2,11 @@
   description = "nixos flake config";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
+      # url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nur = {
