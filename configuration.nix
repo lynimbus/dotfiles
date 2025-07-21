4507c21c00c9093d31f71a403f580d71579e0d22
lantianx <128837704+lantianx233@users.noreply.github.com>
2025-07-21 21:25:39 +0800

    chore(pipewire): disable PulseAudio server emulation

--- configuration.nix
+++ configuration.nix
@@ -96,10 +96,9 @@
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
-    #jack.enable = true;
-
-    #media-session.enable = true;
+    # pulse.enable = true;
+    # jack.enable = true;
+    # media-session.enable = true;
   };
 
   # services.xserver.libinput.enable = true;
