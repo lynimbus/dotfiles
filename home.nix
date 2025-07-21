f80f3430f764f5c4bb63f2e818cba76ef895b8a5
lantianx <128837704+lantianx233@users.noreply.github.com>
2025-07-21 19:05:18 +0800

    refactor: classification of documents in assets/

--- home.nix
+++ home.nix
@@ -11,7 +11,7 @@
   # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
 
   # https://wiki.archlinux.org.cn/title/Tencent_QQ#Empty_login_page_after_a_hot_update
-  home.file.".config/QQ/versions/config.json".source = ./assets/config.json;
+  home.file.".config/QQ/versions/config.json".source = ./assets/QQ/config.json;
 
   # 递归将某个文件夹中的文件，链接到 Home 目录下的指定位置
   # home.file.".config/i3/scripts" = {
@@ -218,7 +218,7 @@
     ];
   };
 
-  home.file.".local/share/fcitx5/rime/default.custom.yaml".source = ./assets/default.custom.yaml;
+  home.file.".local/share/fcitx5/rime/default.custom.yaml".source = ./assets/rime/default.custom.yaml;
 
   home.stateVersion = "25.05";
 }
