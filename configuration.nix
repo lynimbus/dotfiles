23bf898af07888933ddc14d9975a852593a50985
lantianx <128837704+lantianx233@users.noreply.github.com>
2025-07-21 15:14:46 +0800

    feat: add binary cache substituters

--- configuration.nix
+++ configuration.nix
@@ -164,7 +164,20 @@
   nix = {
     settings = {
       experimental-features = ["nix-command" "flakes"];
-      substituters = lib.mkForce ["https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"];
+      substituters = [
+        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+        "https://cache.garnix.io"
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+      trusted-users = ["lantianx"];
       download-buffer-size = 524288000;
       auto-optimise-store = true;
     };
