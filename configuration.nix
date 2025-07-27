e53c9466ef1564001e032c7e5459b8bbd9ee2748
lantianx <lantianx233@gmail.com>
2025-07-27 15:12:16 +0800

    feat(audio): enable pulse jack

--- configuration.nix
+++ configuration.nix
@@ -82,9 +82,8 @@
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    # pulse.enable = true;
-    # jack.enable = true;
-    # media-session.enable = true;
+    pulse.enable = true;
+    jack.enable = true;
   };
 
   # services.xserver.libinput.enable = true;
