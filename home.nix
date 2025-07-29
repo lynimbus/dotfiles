ff8a3c94201bbfe83d360bbecc7c6bf1bcd37bc9
lantianx <lantianx233@gmail.com>
2025-07-29 18:56:03 +0800

    feat: set fish to default shell

--- home.nix
+++ home.nix
@@ -139,7 +139,6 @@
 
   programs.direnv = {
     enable = true;
-    enableBashIntegration = true;
     nix-direnv.enable = true;
   };
 
@@ -214,17 +213,6 @@
   programs.zoxide = {
     enable = true;
     enableFishIntegration = true;
-  };
-
-  programs.bash = {
-    enable = true;
-    initExtra = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
-    '';
   };
 
   programs.fish = {
