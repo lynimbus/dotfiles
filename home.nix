799239125613c7c0f687b080a6f4ce35ae398779
lantianx <lantianx233@gmail.com>
2025-08-01 20:21:22 +0800

    feat(yazi): add nord.yazi full-border.yazi smart-enter.yazi ouch.yazi

--- home.nix
+++ home.nix
@@ -33,7 +33,6 @@
 
   home.packages = with pkgs; [
     broot
-    yazi
 
     fastfetch
     neofetch
@@ -106,6 +105,100 @@
     # wechat
     motrix
   ];
+
+  programs.yazi = {
+    enable = true;
+    enableFishIntegration = true;
+
+    plugins = with pkgs.yaziPlugins; {
+      inherit nord yatline full-border smart-enter ouch;
+    };
+
+    flavors = {inherit (pkgs.yaziPlugins) nord;};
+
+    theme.flavor = {
+      light = "nord";
+      dark = "nord";
+    };
+
+    initLua = ''
+      require("yatline"):setup({
+        theme = require("nord"):setup(),
+      })
+      require("full-border"):setup()
+    '';
+
+    keymap = {
+      mgr.prepend_keymap = [
+        {
+          on = ["l"];
+          run = "plugin smart-enter";
+        }
+        {
+          on = ["<Enter>"];
+          run = "plugin smart-enter";
+        }
+        {
+          on = ["<Right>"];
+          run = "plugin smart-enter";
+        }
+        {
+          on = ["C"];
+          run = "plugin ouch";
+        }
+      ];
+    };
+    settings = {
+      plugin = {
+        prepend_previewers = [
+          {
+            mime = "application/*zip";
+            run = "ouch";
+          }
+          {
+            mime = "application/x-tar";
+            run = "ouch";
+          }
+          {
+            mime = "application/x-bzip2";
+            run = "ouch";
+          }
+          {
+            mime = "application/x-7z-compressed";
+            run = "ouch";
+          }
+          {
+            mime = "application/x-rar";
+            run = "ouch";
+          }
+          {
+            mime = "application/vnd.rar";
+            run = "ouch";
+          }
+          {
+            mime = "application/x-xz";
+            run = "ouch";
+          }
+          {
+            mime = "application/xz";
+            run = "ouch";
+          }
+          {
+            mime = "application/x-zstd";
+            run = "ouch";
+          }
+          {
+            mime = "application/zstd";
+            run = "ouch";
+          }
+          {
+            mime = "application/java-archive";
+            run = "ouch";
+          }
+        ];
+      };
+    };
+  };
 
   programs.direnv = {
     enable = true;
