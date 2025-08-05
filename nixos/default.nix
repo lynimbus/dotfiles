0b99a912b73dcd9fa12bef7b8ffb5ff95955d000
lantianx <lantianx233@gmail.com>
2025-08-05 21:21:21 +0800

    chore: remove hardware config

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,11 +1,6 @@
 {
   imports = [
     ./secrets.nix
-    #./intel-drivers.nix
-    ./amd-drivers.nix
-    ./nvidia-drivers.nix
-    #./optimus-prime-offload-mode.nix
-    ./optimus-prime-sync-mode.nix
     ./gnome.nix
     ./variables.nix
     ./steam.nix
