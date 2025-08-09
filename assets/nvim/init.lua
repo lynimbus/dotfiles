b1b29679f330da210e224b6dca4e1e78180c4f5e
lantianx <lantianx233@gmail.com>
2025-08-09 10:03:37 +0800

    feat(neovim): custom indentation to two spaces

--- assets/nvim/init.lua
+++ assets/nvim/init.lua
@@ -166,6 +166,12 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- Custom indentation to two spaces
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
