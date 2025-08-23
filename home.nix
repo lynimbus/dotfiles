00cd1af09d3fac62d0b256fa20ca6b8d8f72ff8d
lantianx <lantianx233@gmail.com>
2025-08-24 01:23:00 +0800

    feat: enable waylandFrontend

--- home.nix
+++ home.nix
@@ -194,7 +194,7 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    #fcitx5.waylandFrontend = true;
+    fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
       (fcitx5-rime.override {
         rimeDataPkgs =
