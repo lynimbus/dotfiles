ce367d5656bd3fb306ef72f46fb4c03df5de17a9
lantianx <lantianx233@gmail.com>
2025-08-23 13:11:09 +0800

    feat: add flameshot

--- home.nix
+++ home.nix
@@ -67,6 +67,22 @@
     motrix
   ];
 
+  services.flameshot = {
+    enable = true;
+    settings = {
+      General = {
+        uiColor = "#ff78c5";
+        disabledTrayIcon = true;
+        useGrimAdapter = true;
+        showHelp = false;
+        showSidePanelButton = false;
+        showDesktopNotification = false;
+        showAbortNotification = false;
+        showStartupLaunchMessage = false;
+      };
+    };
+  };
+
   programs.kitty = {
     enable = true;
     font = {
