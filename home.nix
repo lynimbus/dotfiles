98dc6fbd654a350e7c18ec026a8df839621bd2a9
lantianx <lantianx233@gmail.com>
2025-08-26 10:14:08 +0800

    feat(kitty): add kitty settings

--- home.nix
+++ home.nix
@@ -88,6 +88,17 @@
       size = 14;
     };
     themeFile = "Dracula";
+    settings = {
+      window_border_width = "0px";
+      tab_bar_edge = "top";
+      tab_bar_margin_width = "0.0";
+      tab_bar_style = "fade";
+      placement_strategy = "top-left";
+      confirm_os_window_close = 0;
+      remember_window_size = "no";
+      initial_window_width = 800;
+      initial_window_height = 600;
+    };
   };
 
   programs.direnv = {
