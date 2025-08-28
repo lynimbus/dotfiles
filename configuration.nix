09ed289bb564765675a06f8e0cde4083a96f2fc9
lantianx <lantianx233@gmail.com>
2025-08-29 03:34:01 +0800

    feat(fcitx5): add fcitx5-qt

--- configuration.nix
+++ configuration.nix
@@ -134,6 +134,26 @@
     nerd-fonts.jetbrains-mono
   ];
 
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.waylandFrontend = true;
+    fcitx5.addons = with pkgs; [
+      fcitx5-configtool
+      fcitx5-gtk
+      kdePackages.fcitx5-qt
+      fcitx5-material-color
+      kdePackages.fcitx5-chinese-addons
+      (fcitx5-rime.override {
+        rimeDataPkgs =
+          with pkgs.nur.repos.linyinfeng.rimePackages;
+          withRimeDeps [
+            rime-ice
+          ];
+      })
+    ];
+  };
+
   services.dae = {
     enable = true;
     package = inputs.daeuniverse.packages.x86_64-linux.dae-unstable;
--- home.nix
+++ home.nix
@@ -190,22 +190,6 @@
     };
   };
 
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5.waylandFrontend = true;
-    fcitx5.addons = with pkgs; [
-      (fcitx5-rime.override {
-        rimeDataPkgs =
-          with pkgs.nur.repos.linyinfeng.rimePackages;
-          withRimeDeps [
-            rime-ice
-          ];
-      })
-      fcitx5-gtk
-      fcitx5-configtool
-    ];
-  };
   home.file.".local/share/fcitx5/rime/default.custom.yaml".source = ./assets/rime/default.custom.yaml;
 
   # https://wiki.archlinux.org.cn/title/Tencent_QQ#Empty_login_page_after_a_hot_update
