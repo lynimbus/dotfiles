3683409b0a2c97e868e534133d7e1ed197a0ec44
lantianx <lantianx233@gmail.com>
2025-09-22 00:29:09 +0800

    feat(terminal): add alacritty

--- home/terminal.nix
+++ home/terminal.nix
@@ -6,17 +6,23 @@
       name = "JetBrainsMono Nerd Font Mono";
       size = 14;
     };
-    themeFile = "Dracula";
+    themeFile = "GitHub_Dark";
     settings = {
-      window_border_width = "0px";
-      tab_bar_edge = "top";
-      tab_bar_margin_width = "0.0";
-      tab_bar_style = "fade";
-      placement_strategy = "top-left";
       confirm_os_window_close = 0;
       remember_window_size = "no";
       initial_window_width = 800;
       initial_window_height = 600;
     };
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        size = 14;
+        normal.family = "JetBrainsMono Nerd Font Mono";
+      };
+    };
+    theme = "github_dark";
   };
 }
