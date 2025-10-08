f9e945e9994e93f6e1697e2f5dd7b4730e8f1538
lantianx <lantianx233@gmail.com>
2025-10-09 04:39:14 +0800

    feat(flake): 切换到自己的nixos-hardware仓库

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
     };
 
     kickstart-nixvim.url = "github:JMartJonesy/kickstart.nixvim";
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    nixos-hardware.url = "github:lynimbus/nixos-hardware";
     import-tree.url = "github:vic/import-tree";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
