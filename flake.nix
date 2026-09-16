a986e6e0d3afddd26aa41e1eaaa4084de04c2467
lynimbus <128837704+lynimbus@users.noreply.github.com>
2026-09-16 17:53:43 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "lynimbus's nixos flake configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    let
+      hostname = "nixos";
+      username = "lynimbus";
+      email = "128837704+lynimbus@users.noreply.github.com";
+    in
+    {
+      nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs hostname username; };
+        modules = [
+          ./system/init.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs username email; };
+            home-manager.users.${username} = ./home/init.nix;
+            home-manager.backupFileExtension = "old";
+          }
+        ];
+      };
+    };
+}
---
+++ system/init.nix
@@ -0,0 +1,5 @@
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  system.stateVersion = "26.05";
+}
