4c5a73810b4082921caf105a6fa1980bbe490cf4
lynimbus <128837704+lynimbus@users.noreply.github.com>
2026-09-23 23:50:21 +0800

    chore(input-method): 移除冗余的 fcitx5 插件包

--- system/input-method.nix
+++ system/input-method.nix
@@ -8,11 +8,6 @@
       waylandFrontend = true;
       ignoreUserConfig = true;
       addons = with pkgs; [
-        kdePackages.fcitx5-configtool
-        fcitx5-gtk
-        kdePackages.fcitx5-qt
-        kdePackages.fcitx5-chinese-addons
-        fcitx5-material-color
         qingjian
         (fcitx5-rime.override {
           rimeDataPkgs = [ rime-ice ];
